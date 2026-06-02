a3aa12d67a41a1ac046f05dff6dd2b911e61cb9b
Ezequiel <ezequielcs92@gmail.com>
2026-06-02 12:01:11 -0300

    Add contact information constants and update footer, layout, and contact pages to use dynamic data

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,19 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const CONTACT_INFO = {
+	email: 'twinboxmex@gmail.com',
+	whatsappPrimaryPhone: '987-111-6712',
+	whatsappPrimaryLabel: '+52 987 111 6712',
+	whatsappPrimaryUrl: 'https://wa.me/529871116712',
+	secondaryPhone: '987-800-4033',
+	secondaryLabel: '+52 987 800 4033',
+	secondaryWhatsAppUrl: 'https://wa.me/529878004033',
+	facebook: 'https://www.facebook.com/TwinboxMexico',
+	instagram: 'https://www.instagram.com/twinboxmexico/',
+	youtube: 'https://www.youtube.com/@twinboxmexico',
+	tiktok: 'https://www.tiktok.com/@twinbox.publicidad',
+	x: 'https://x.com/twinboxm',
+	googleBusinessProfile: 'https://share.google/M2cCoiAjKQrGvGYlA',
+};
